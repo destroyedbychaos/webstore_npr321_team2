ebdc6b96f8537fef6d91e5c90088405ceb505d7f
maxim <167625699+RiXeN12@users.noreply.github.com>
2025-02-27 21:57:52 +0200

    Add shoppingContext

--- webstore_front/src/App.jsx
+++ webstore_front/src/App.jsx
@@ -3,13 +3,17 @@ import "./App.css";
 import { BrowserRouter } from 'react-router-dom'
 import BasicRoute from './routes/BasicRoute'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { ShoppingProvider } from './context/ShoppingContext';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <BasicRoute />
-    </BrowserRouter>
+      <ShoppingProvider>
+          <BrowserRouter>
+              <BasicRoute />
+          </BrowserRouter>
+      </ShoppingProvider>
+    
   )
 }
 
