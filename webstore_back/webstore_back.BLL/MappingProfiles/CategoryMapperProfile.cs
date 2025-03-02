3b5d9873978427d7f4373fc799491c790824837b
andromaan <122400140+andr0man@users.noreply.github.com>
2025-03-02 10:47:22 +0200

    fix mapping profiles

--- webstore_back/webstore_back.BLL/MappingProfiles/CategoryMapperProfile.cs
+++ webstore_back/webstore_back.BLL/MappingProfiles/CategoryMapperProfile.cs
@@ -8,22 +8,26 @@ namespace webstore_back.BLL.MappingProfiles
 {
     public class CategoryMapperProfile : Profile
     {
-        public CategoryMapperProfile(ICategoryService _categoryService)
+        // public CategoryMapperProfile(ICategoryService _categoryService)
+        // {
+        //     CreateMap<Category, CategoryVM>()
+        //         .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+        //         .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+        //
+        //     CreateMap<CategoryVM, Category>()
+        //         .ForMember(dest => dest.ClothingItems, opt => opt.Ignore())
+        //         .AfterMap(async (src, dest) =>
+        //         {
+        //                var response = await _categoryService.GetCategoryClothingItemsById(src.Id);
+        //                if (response.Payload is Category category)
+        //                {
+        //                    dest.ClothingItems = category.ClothingItems;
+        //                }
+        //         });
+        // }
+        public CategoryMapperProfile()
         {
-            CreateMap<Category, CategoryVM>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
-
-            CreateMap<CategoryVM, Category>()
-                .ForMember(dest => dest.ClothingItems, opt => opt.Ignore())
-                .AfterMap(async (src, dest) =>
-                {
-                       var response = await _categoryService.GetCategoryClothingItemsById(src.Id);
-                       if (response.Payload is Category category)
-                       {
-                           dest.ClothingItems = category.ClothingItems;
-                       }
-                });
+            CreateMap<Category, CategoryVM>().ReverseMap();
         }
     }
 }
